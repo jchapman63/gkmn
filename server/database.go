86bbb1dfe342d68e208905703212d204bae57ca7
Jordan Chapman <jordan.chapman@immuta.com>
2024-03-18 09:33:19 -0400

    connect to pg container

---
+++ server/database.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func TestConnection() {
+	// connect to pre created user and database (created to test out pgsql in a docker container)
+	connectionStr := "postgres://docker_user/docker_user@localhost:5432/docker_user"
+
+	conn, err := sql.Open("postgres", connectionStr)
+	if err != nil {
+		panic(err)
+	}
+
+	conn.Close()
+}
+
+// connection string format
+// postgres://user:password@host/database
