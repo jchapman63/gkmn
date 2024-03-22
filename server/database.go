ff017a14c7bfdd2e88b387dbe8876fec00eed5bc
Jordan Chapman <jordan.chapman@immuta.com>
2024-03-21 20:29:38 -0400

    i am not sure here

--- server/database.go
+++ server/database.go
@@ -2,17 +2,29 @@ package server
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
 func TestConnection() {
 	// connect to pre created user and database (created to test out pgsql in a docker container)
-	connectionStr := "postgres://docker_user/docker_user@localhost:5433/docker_user"
+	connectionStr := "postgres://docker_user:docker_user@localhost:5433/docker_user?sslmode=disable"
 
 	conn, err := sql.Open("postgres", connectionStr)
 	if err != nil {
 		panic(err)
+	}
+
+	rows, err := conn.Query("SELECT version();")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var version string
+		rows.Scan(&version)
+		fmt.Println(version)
 	}
 
 	conn.Close()
--- server/database_test.go
+++ server/database_test.go
@@ -2,8 +2,6 @@ package server
 
 import "testing"
 
-// container must be running locally for this to work ? or maybe not ?
-// this test passes when the container isn't running ? PGADMIN will not connect
 func TestDataBaseConnection(t *testing.T) {
 	TestConnection()
 }
